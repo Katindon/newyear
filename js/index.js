0d6a5e299a8c73a23dfadd844d83e2b604030425
Katindon <34502978+Katindon@users.noreply.github.com>
2017-12-23 09:16:09 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ window.onload=function(){
 	var page1 = document.getElementById("page1");
 	var page2 = document.getElementById("page2");
 	var page3 = document.getElementById("page3");
+	var p1_lantern = document.getElementsByClassName("p1_lantern")[0];
 
 	var music = document.getElementById("music");
 	var audio = document.getElementsByTagName("audio")[0];
@@ -47,7 +48,7 @@ window.onload=function(){
 		}
 	},false);
 
-	page1.addEventListener("touchstart",function(event){
+	p1_lantern.addEventListener("touchstart",function(event){
 		page1.style.display = "none";
 		page2.style.display = "block";
 		page3.style.display = "block";
@@ -55,6 +56,6 @@ window.onload=function(){
 		setTimeout(function(){
 			page2.setAttribute("class","page fadeOut")
 			page3.setAttribute("class","page fadeIn")
-		},6500)
+		},5500)
 	},false);
 };
